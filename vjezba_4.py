4495cbd6d8864a4f9414cb15e792f4f37ce086d8
zrupic <zr.test.ap@gmail.com>
2024-11-15 00:07:24 +0100

    Vjezbe 4 i 6

---
+++ vjezba_4.py
@@ -0,0 +1,23 @@
+broj = 0
+while broj < 5:
+ broj +=2
+print(broj)
+
+# ispisuje broj 6
+
+broj = 0
+while broj < 5:
+ broj += 1
+ print(broj)
+ broj -= 1
+
+ # beskonačna je jer ju povečajemo i nakon toga smanjujemo za 1
+
+broj = 10
+while broj > 0:
+ broj -= 1
+ print(broj)
+if broj < 5:
+ broj += 2
+
+ #nema izlaza iz petlje 
