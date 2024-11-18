02d31311e7d956084e24c7a8a02964dcbcb1aa96
zrupic <zr.test.ap@gmail.com>
2024-11-18 23:52:00 +0100

    Novi floder s vježbama

---
+++ RS2/vjezba1.py
@@ -0,0 +1,16 @@
+kvadriraj =lambda x: x ** 2
+print (kvadriraj(5))
+
+zbr_kvadriraj= lambda a,b: (a + b)**2
+print(zbr_kvadriraj(2, 3))
+
+
+niz_kvadriraj = lambda niz: len(niz)**2
+print(niz_kvadriraj("raspodjeljeni"))
+       
+
+pom_potenciraj= lambda x, y:(5*y)**x
+print(pom_potenciraj(2, 5))
+
+paran_broj = lambda x: "True" if x % 2 == 0 else "None"
+print(paran_broj(3))
